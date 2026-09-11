6382f1eeb1ba3f45634498ecaf144a673b073435
Amina <sapokkot3@gmail.com>
2026-09-11 12:30:37 +0500

    first practice

---
+++ practice1/src/main/java/com/example/practice1/helloWorld.java
@@ -0,0 +1,15 @@
+package com.example.practice1;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class helloWorld {
+
+    @GetMapping("/get/helloWorld")
+    public String getHelloWorld(@RequestBody String name){
+        return "Hello world! " + name;
+    }
+
+
+}
